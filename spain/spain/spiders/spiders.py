d8a32b80f1d893f1d3d2e5a05795b0b795648402
Rasend <rasend20@gmail.com>
2023-04-19 18:46:04 -0700

    anadiendo requirements.txt y terminando de pulir el script

--- spain/spain/spiders/spiders.py
+++ spain/spain/spiders/spiders.py
@@ -1,29 +1,62 @@
 import scrapy
 import pandas as pd
 import time
-# page_name = str(input('ingrese la institucion que desea investigar: \n'))
-# state = str(input('ingrese el estado en donde se ejecutara la busqueda: \n'))
+
+state = str(input('ingrese el estado en donde se ejecutara la busqueda: \n'))
+target = str(input('ingrese la institucion que desea investigar: \n'))
+
+sentences = {'names': '//div[@class="box"]//h2/span[@itemprop="name"]/text()',
+             'address': '//div[@class="box"]//span[@itemprop="streetAddress"]/text()',
+             'postal_code': '//div[@class="box"]//span[@itemprop="postalCode"]/text()',
+             'phone': '//div[@class="box"]//span[@itemprop="telephone"]/text()'}
 
 
 class PaginasAmarillas(scrapy.Spider):
     name = 'amarillas'
     start_urls = [
-        f'https://www.paginasamarillas.es/a/colegios-privados/guadalajara/']
+        f'https://www.paginasamarillas.es/a/{target}/{state}/']
+    data = {'name': [], 'address': [],
+            'postal_code': [], 'phone': [], 'state': []}
 
     def parse(self, response):
         names = response.xpath(
-            '//div[@class="box"]//h2/span[@itemprop="name"]/text()').getall()
+            f'{sentences["names"]}').getall()
         address = response.xpath(
-            '//div[@class="box"]//span[@itemprop="streetAddress"]/text()').getall()
+            f'{sentences["address"]}').getall()
         postal_code = response.xpath(
-            '//div[@class="box"]//span[@itemprop="postalCode"]/text()').getall()
+            f'{sentences["postal_code"]}').getall()
         phone = response.xpath(
-            '//div[@class="box"]//span[@itemprop="telephone"]/text()').getall()
+            f'{sentences["phone"]}').getall()
 
-        for i, v in enumerate(names):
-            time.sleep(1)
-            print(
-                f'nombre: {v}\n direccion: {address[i]}\n codigo postal: {postal_code[i]}\n phone: {phone[i]}\n\n')
+        # esta sentencia xpath hace referencia al boton de siguiente pagina
+        if response.xpath('//i[contains(@class,"flecha-derecha")]'):
+            next_link = response.xpath(
+                '//i[contains(@class,"flecha-derecha")]/../@href').get()
+            self.data['name'].extend(names)
+            self.data['address'].extend(address)
+            self.data['postal_code'].extend(postal_code)
+            self.data['phone'].extend(phone)
+            yield response.follow(f'{next_link}', callback=self.next_page)
+        else:
+            self.data['state'].extend([state for i in self.data['name']])
+            df = pd.DataFrame(self.data)
+            df.to_excel(f'{target}_paginas_amarillas_spain.xlsx', index=False)
 
-        # <span class="location" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress"><span itemprop="streetAddress">Avenida Burgos, 3</span><span itemprop="postalCode">19005</spa
-        # n><span itemprop="addressLocality">Guadalajara</span></span>
+    def next_page(self, response):
+        self.data['name'].extend(response.xpath(
+            f'{sentences["names"]}').getall())
+        self.data['address'].extend(response.xpath(
+            f'{sentences["address"]}').getall())
+        self.data['postal_code'].extend(response.xpath(
+            f'{sentences["postal_code"]}').getall())
+        self.data['phone'].extend(response.xpath(
+            f'{sentences["phone"]}').getall())
+
+        if response.xpath('//i[contains(@class,"flecha-derecha")]'):
+            next_link = response.xpath(
+                '//i[contains(@class,"flecha-derecha")]/../@href').get()
+            yield response.follow(f'{next_link}', callback=self.next_page)
+        else:
+            self.data['state'].extend([state for i in self.data['name']])
+            df = pd.DataFrame(self.data)
+            df.to_excel(f'{target}_paginas_amarillas_spain.xlsx', index=False)

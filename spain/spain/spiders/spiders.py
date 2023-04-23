7b5a8e19d2dbff534ec26c06fef8d580443510cf
Rasend <rasend20@gmail.com>
2023-04-23 10:07:11 -0700

    modificando el archivo para poder abrir el excel con cada iteracion y anadir datos a este

--- spain/spain/spiders/spiders.py
+++ spain/spain/spiders/spiders.py
@@ -1,7 +1,6 @@
 import scrapy
 import pandas as pd
 import re
-import time
 
 state = str(input('ingrese el estado en donde se ejecutara la busqueda: \n'))
 target = str(input('ingrese la institucion que desea investigar: \n'))
@@ -14,6 +13,8 @@ sentences = {'names': '//h1/text()',
 
 patron_re = r'"customerMail":"([^"]+)"'
 
+filename = f'{target}_paginas_amarillas_spain_{state}.xlsx'
+
 
 class PaginasAmarillas(scrapy.Spider):
     name = 'amarillas'
@@ -21,7 +22,7 @@ class PaginasAmarillas(scrapy.Spider):
         f'https://www.paginasamarillas.es/a/{target}/{state}/']
     data = {'name': [], 'address': [],
             'postal_code': [], 'phone': [], 'email': [], 'state': []}
-    links_2 = []
+
     def parse(self, response):
         links = response.xpath(
             '//a[@href and @title and @data-omniclick="name"]/@href').getall()
@@ -31,18 +32,15 @@ class PaginasAmarillas(scrapy.Spider):
             next_link = response.xpath(
                 '//i[contains(@class,"flecha-derecha")]/../@href').get()
             # llamamos a nuestra misma funcion para poder seguir obteniendo links de las paginas
-            yield response.follow(next_link, callback=self.other_parse,cb_kwargs={'links': links})
+            yield response.follow(next_link, callback=self.other_parse, cb_kwargs={'links': links})
         else:
             # una vez tenemos todos los links procedemos a ejecutar la funcion in_page para investigarlos a fondo
-            while links:
-                if len(links) == 1:
-                    df = pd.DataFrame(self.data)
-                    df.to_excel(f'{target}_paginas_amarillas_spain_{state}.xlsx', index=False)
-                    break
-                else:
-                    yield response.follow(links.pop(), callback=self.gather_page_info)
+            df = pd.DataFrame(self.data)
+            df.to_excel(filename, index=False)
+            for i in links:
+                yield response.follow(i, callback=self.gather_page_info)
 
-    def other_parse(self,response,**kwargs):
+    def other_parse(self, response, **kwargs):
         links = kwargs['links']
         links.extend(response.xpath(
             '//a[@href and @title and @data-omniclick="name"]/@href').getall())
@@ -52,33 +50,32 @@ class PaginasAmarillas(scrapy.Spider):
             next_link = response.xpath(
                 '//i[contains(@class,"flecha-derecha")]/../@href').get()
             # llamamos a nuestra misma funcion para poder seguir obteniendo links de las paginas
-            yield response.follow(next_link, callback=self.other_parse,cb_kwargs={'links': links})
+            yield response.follow(next_link, callback=self.other_parse, cb_kwargs={'links': links})
         else:
             # una vez tenemos todos los links procedemos a ejecutar la funcion in_page para investigarlos a fondo
-            self.links_2 = links
+            df = pd.DataFrame(self.data)
+            df.to_excel(filename, index=False)
             for i in links:
                 yield response.follow(i, callback=self.gather_page_info)
 
-
     def gather_page_info(self, response):
-        print(len(self.links_2))
-        self.links_2.pop()
-        if len(self.links_2) == 1:
-            df = pd.DataFrame(self.data)
-            df.to_excel(f'{target}_paginas_amarillas_spain_{state}.xlsx', index=False)
+        self.data['name'].append(response.xpath(
+            f'{sentences["names"]}').get())
+        self.data['address'].append(response.xpath(
+            f'{sentences["address"]}').get())
+        self.data['postal_code'].append(response.xpath(
+            f'{sentences["postal_code"]}').get())
+        self.data['phone'].append(response.xpath(
+            f'{sentences["phone"]}').get())
+        self.data['state'].append(state)
+        email_text = response.xpath(sentences['email']).get()
+        if email_text:
+            captured_mail = re.search(patron_re, email_text).group(1)
+            self.data['email'].append(captured_mail)
         else:
-            self.data['name'].append(response.xpath(
-                f'{sentences["names"]}').get())
-            self.data['address'].append(response.xpath(
-                f'{sentences["address"]}').get())
-            self.data['postal_code'].append(response.xpath(
-                f'{sentences["postal_code"]}').get())
-            self.data['phone'].append(response.xpath(
-                f'{sentences["phone"]}').get())
-            self.data['state'].append(state)
-            email_text = response.xpath(sentences['email']).get()
-            if email_text:
-                captured_mail = re.search(patron_re, email_text).group(1)
-                self.data['email'].append(captured_mail)
-            else:
-                self.data['email'].append('no_encontrado')
+            self.data['email'].append('no_encontrado')
+
+        # adding to the file section
+        df = pd.read_excel(filename)
+        df.append(self.data, ignore_index=True)
+        df.to_excel(filename, index=False)
